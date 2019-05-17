5650705da16070fb9703925f6856dce5a7b803d9
Dilip Kumar <dilkumar@google.com>
2019-05-17 16:26:41 +0530

    first committt

--- src/main/java/todo/com/todo/TodoController.java
+++ src/main/java/todo/com/todo/TodoController.java
@@ -2,8 +2,10 @@ package todo.com.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,10 +33,21 @@ public class TodoController{
         return todo;
     }
 
-    @PutMapping("/todos/{id}")
-    public void save(@RequestBody Todo todo, @PathVariable int id){
-
-        System.out.print(todo.getTitle());
+    @PutMapping("/todos")
+    public void updateTodo(@RequestBody Todo todo){
+        
         todoRepo.save(todo);
+    }
+
+    @PostMapping("/todos")
+    public void saveTodo(@RequestBody Todo todo){
+        
+        todoRepo.save(todo);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteTodo(@PathVariable int id){
+        
+        todoRepo.deleteById(id);;
     }
 }
